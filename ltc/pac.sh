34d6a82d100eee0615261485eb3ff70e585a1d4e
Soul Calibre <52732022+SoulCalibre003@users.noreply.github.com>
2019-07-13 05:43:49 +0000

    Update pac.sh

--- ltc/pac.sh
+++ ltc/pac.sh
@@ -11,8 +11,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo
-echo -e "\e[1n Subscribe to Termux Tricks & Tut
-Kung merong katanungan mag PM sa aking FB account fb.me/jonabine"
 echo
 echo -e "\e[1m\e[32m Enter :\e[33m exit"
 echo
