27097a5539caab19a010a75ef126543cbcba2eae
Soul Calibre <52732022+SoulCalibre003@users.noreply.github.com>
2019-07-13 17:03:57 +0000

    Create ltc1.py

---
+++ ltc/ltc1.py
@@ -0,0 +1,78 @@
+B
+
+    Iýí\§$  ã               @   s   d dl Z ee  d¡ƒ dS )é    Ns±  ã               @   sÎ  d dl mZmZmZ d dlmZmZ d dlmZ d dlm	Z	 d dl
+mZ d dlZd dl
+Z
+d dlZd dlZyd dlZd dlmZ W n   edƒ e ¡  Y nX d	Zej d
+¡s¸e d
+¡ eeƒ eejƒdk ràedƒ e d
+¡ dd„ Ze ¡ ZddiZdZdZ ejd
+ Z!ede! ee ƒZ"e" #¡  e" $¡ svye" %e!¡ e" &e!e'dƒ¡Z(W n* ek
+rt   e'dƒZ)e" *e!e)¡Z(Y nX e" +¡ Z,e -d¡ eeƒ ede,j.dƒ edƒ ze" /d¡Z0dZ1x e2dƒD ]òZ3ej4 5d¡ ej4 5d¡ ej4 5d¡ ej4 5d¡ ej4 6¡  e"j7e0d d! ed"ƒ e"ee0d
+dd d d d d d#ƒZ8e8j9d  j: ;d$¡d%krbed&ƒ e ¡  qÄyze8j9d  j<j=d  j>d  j?Z?ej4 5d¡ ej4 5d'e? ¡ ej4 6¡  e8j9d  j@Z@ejAe?ed(d)d*ZBeeBjCd+ƒZDeDj;d,d-d.dkrÆeDj;d,d/d0dkrÆedƒ e"ee0d
+dd d d d d d#ƒZ8e8j9d  j:Z:e8j9d  j: ;d1¡d%ksXe8j9d  j: ;d2¡d%krÜe
+ Ed3e:¡ZFeeGeFd  ƒƒ ed
+ƒ e"ee0ddd d d d d d#ƒZ8e8j9d
+ j:ZHedƒ ej4 5d4eH d5 ¡ n neDj;d,d/d0dk	r^x€eDjId,d6d.D ]nZJeJ Ad7¡ZKeJ Ad8¡ZLeJ Ad9¡ZMeeGeLƒƒ ejNd:eKeMd;œed(d)d<ZBe OeBjP¡ZQej4 5d=eQd>  d? ¡ qêW n~ej4 5d¡ ej4 5d@¡ ej4 5d¡ ej4 5dA¡ ej4 6¡  edƒ e"ee1e@e8j9d  j<j=d  j>d
+ jRdBƒ ej4 5dC¡ edƒ W nÖ   ed"ƒ e"ee0d
+dd d d d d d#ƒZ8e8j9d  j:Z:e8j9d  j: ;d1¡d%ksFe8j9d  j: ;d2¡d%kr°e
+ Ed3e:¡ZFeeGeFd  ƒƒ ed
+ƒ e"ee0ddd d d d d d#ƒZ8e8j9d
+ j:ZHedƒ ej4 5d4eH d5 ¡ n Y nX qÄW W de" S¡  X dS )Dé    )ÚTelegramClientÚsyncÚevents)ÚGetHistoryRequestÚGetBotCallbackAnswerRequest)ÚSessionPasswordNeededError)ÚFloodWaitError)ÚsleepN)Ú
+BeautifulSoupz—[1;30m# [1;31mHmmm Sepertinya Modul Requests Dan Bs4 Belum Terinstall
+[1;30m# [1;31mTo install Please Type pip install requests and pip install bs4aT  [0;35m
+ ######     #####    ##     ##  ##
+##   ##   ##     ##  ##     ##  ##
+##        ##     ##  ##     ##  ##
+ ######   ##     ##  ##     ##  ##
+      ##  ##     ##  ##     ##  ##
+##    ##  ##     ##  ##     ##  ##
+ ######     #####      #####    ######
+[0;34m=========================================================
+[1;32mAuthor By  [1;31m:[1;0m SoulCalibre
+[1;32mFacebook Page[1;31m : [1;0mSoulCalibre007
+
+
+[1;32mUsage : python ltc1.py +62é   c             C   sb   t j d¡ t j d¡ xDt| ddƒD ]4}t j d¡ t j d |¡¡ t j ¡  tdƒ q&W d S )Nú
+z?                                                               r   éÿÿÿÿz,[1;30m#[1;0m{:2d} [1;32mseconds remainingr   )ÚsysÚstdoutÚwriteÚrangeÚformatÚflushr	   )ÚxZ	remaining© r   Ú Útunggu!   s    
+r   z
+User-Agentz•Mozilla/5.0 (Linux; Android 5.1; A1603 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36iqò
+ Z 322526d2c3350b1d3530de327cf08c07zsession/z 
+
+
+[1;0mEnter Yout Code Code : z[1;0mYour 2fa Password : Úclearz[1;32mWelcome To TeleBotz8
+[1;32mBot Ini Di Gunakan Untuk Menuyul LTC Click Bot
+
+z[1;37mPlease wait......!z@Litecoin_click_boti@KL r
+   z>                                                              z%[1;30m# [1;33mMencoba Mengambil URLu   ðŸ–¥ Visit sites)ZentityÚmessageé   )ZpeerZlimitZoffset_dateZ	offset_idZmax_idZmin_idZ
+add_offsetÚhashz%Sorry, there are no new ads availabler   z3
+[1;30m# [1;31mIklan Sudah Habis Coba Lagi Besok
+z[1;30m# [1;33mVisit é   T)ÚheadersÚtimeoutÚallow_redirectszhtml.parserZdivzg-recaptcha)Zclass_Zheadbar)Úidz
+You must stayzPlease stay onz([\d.]*\d+)z
+[1;30m# [1;32mÚ
+zcontainer-fluidz	data-codez
+data-timerz
+data-tokenzhttps://dogeclick.com/reward)ÚcodeZtoken)Údatar   r   r    z
+[1;30m# [1;32mYou earned Zrewardz LTC for visiting a site!
+z@                                                                z [1;30m# [1;31mCaptcha Detected)r$   z)
+[1;30m# [1;31mSkip Captcha...!       
+)TZtelethonr   r   r   Ztelethon.tl.functions.messagesr   r   Ztelethon.errorsr   r   Ztimer	   ZjsonÚrer   ÚosZrequestsZbs4r
+   ÚprintÚexitZbannerÚpathÚexistsÚmakedirsÚlenÚargvr   ZSessionÚcZuaZapi_idZapi_hashZphone_numberZclientZconnectZis_user_authorizedZsend_code_requestZsign_inÚinputÚmeZpasswÚstartZget_meZmyselfÚsystemZ
+first_nameZ
+get_entityZchannel_entityZchannel_usernamer   Úir   r   r   Zsend_messageZpostsZmessagesr   ÚfindZreply_markupZrowsZbuttonsZurlr!   ÚgetÚrZcontentZsoupZfindallZsecÚintZ
+messageresZfind_allZdatr#   ZtimerZtokenaZpostÚloadsÚtextZjsr$   Z
+disconnectr   r   r   r   Ú<module>   sÚ    
+
+
+
+
+
+
+
+
+
+(0
+
+
+ 
+ 0)ÚmarshalÚexecÚloads© r   r   úout.pyÚ<module>   s   
